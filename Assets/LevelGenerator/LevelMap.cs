2fa5ecd9c671abe5431922fa5f1e0f15acd9c287
Ainius <giliaidzin@gmail.com>
2017-01-22 01:34:44 +0200

    prefab tests

--- Assets/LevelGenerator/LevelMap.cs
+++ Assets/LevelGenerator/LevelMap.cs
@@ -10,6 +10,7 @@ namespace Assets.LevelGenerator
 	{
 		private List<PropsPattern> m_PropsPatterns = new List<PropsPattern>();
 		private List<RoomPattern> m_RoomPatterns = new List<RoomPattern>();
+		private Dictionary<string, Transform> m_Prefabs = new Dictionary<string, Transform>();
 
 		public Transform OutCornerTopLeftTile;
 		public Transform OutCornerTopRightTile;
@@ -25,13 +26,36 @@ namespace Assets.LevelGenerator
 		public Transform WallBottomTile;
 		public Transform EmptyTile;
 
+		//public Dictionary<string, Transform> Prefabs { get { return m_Prefabs; } }
+
+		//public Dictionary<string, Transform> Prefabs2;
+		//public Transform[] Prefabs3;
+
 		void Start()
 		{
+			InitPrefabs();
 			LoadRoomPatters();
 			LoadPropPatterns();
 			GenerateMap();
 
-			TestLevel();
+			//TestLevel();
+		}
+
+		void InitPrefabs()
+		{
+			m_Prefabs.Add("OutCornerTopLeft", OutCornerTopLeftTile);
+			m_Prefabs.Add("OutCornerTopRight", OutCornerTopRightTile);
+			m_Prefabs.Add("OutCornerBottomRight", OutCornerBottomRightTile);
+			m_Prefabs.Add("OutCornerBottomLeft", OutCornerBottomLeftTile);
+			m_Prefabs.Add("InCornerTopLeft", InCornerTopLeftTile);
+			m_Prefabs.Add("InCornerTopRight", InCornerTopRightTile);
+			m_Prefabs.Add("InCornerBottomRight", InCornerBottomRightTile);
+			m_Prefabs.Add("InCornerBottomLeft", InCornerBottomLeftTile);
+			m_Prefabs.Add("WallLeft", WallLeftTile);
+			m_Prefabs.Add("WallTop", WallTopTile);
+			m_Prefabs.Add("WallRight", WallRightTile);
+			m_Prefabs.Add("WallBottom", WallBottomTile);
+			m_Prefabs.Add("Empty", EmptyTile);
 		}
 
 		void TestLevel()
@@ -94,6 +118,29 @@ namespace Assets.LevelGenerator
 
 		public void GenerateMap()
 		{
+			float cellSize = 2.56f;// 0.7f;
+			RoomPattern room = m_RoomPatterns[2];
+			int roomWidth = room.Width;
+			int roomHeight = room.Height;
+			float startX = -cellSize * (roomWidth / 2.0f);
+			float startY = cellSize * (roomHeight / 2.0f);
+			foreach (FloorTile tile in room.Tiles)
+			{
+				Vector3 pos = new Vector3(startX + cellSize * tile.XIndex, startY - cellSize * tile.YIndex, 0.0f);
+				if (m_Prefabs.ContainsKey(tile.FloorSprite))
+				{
+					Transform prefab = m_Prefabs[tile.FloorSprite];
+					Instantiate(prefab, pos, Quaternion.identity, transform);
+				}
+			}
+			//for (int i = 0; i < roomWidth; ++i)
+			//{
+			//	for (int j = 0; j < roomHeight; ++j)
+			//	{
+			//		Vector3 pos = new Vector3(startX + cellSize * i, startY + cellSize * j, 0.0f);
+			//		//room.Tiles
+			//	}
+			//}
 		}
 
 		public void LoadRoomPatters()
@@ -101,8 +148,9 @@ namespace Assets.LevelGenerator
 			string[] files =
 			{
 				"SmallRoom",
-				//"LevelGenerator/BigRoom.xml",
-				//"LevelGenerator/CorridorRoom.xml"
+				"BigRoom",
+				"CrossSection",
+				//"CorridorRoom"
 			};
 			foreach (string patternFile in files)
 			{
