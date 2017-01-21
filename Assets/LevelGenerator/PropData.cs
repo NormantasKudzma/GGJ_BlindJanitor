ebf9f9ca56ed1289009030f2288904e25336519e
Ainius <giliaidzin@gmail.com>
2017-01-21 18:55:59 +0200

    Level generator initial files

---
+++ Assets/LevelGenerator/PropData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.LevelGenerator
+{
+	public class PropData
+	{
+		public string SpriteName { set; get; }
+		public Vector2 SpriteOffset { set; get; }
+	}
+}
