61f4d11aae7e62f5c74e06e5dde46b905b6e927f
Normantas <kudzma.normantas@gmail.com>
2017-01-22 09:12:37 +0200

    Fixing git project according to http://stackoverflow.com/questions/21573405/how-to-prepare-a-unity-project-for-git

---
+++ Assets/OldPlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OldPlayerMovement : MonoBehaviour {
+
+	float m_MaxSpeed = 0.09f;
+	const float m_MaxLightDuration = 0.5f;
+	float m_LightDuration = m_MaxLightDuration;
+
+	// Use this for initialization
+	void Start () {
+		tag = "Player";
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+
+		pos.x += Input.GetAxis("Horizontal") * m_MaxSpeed;
+		pos.z += Input.GetAxis("Vertical") * m_MaxSpeed;
+
+		transform.position = pos;
+
+		if (m_LightDuration > 0.0f) 
+		{
+			m_LightDuration -= Time.deltaTime;
+
+			if (m_LightDuration <= 0.0f) 
+			{
+				GetComponent<BoxCollider> ().enabled = false;
+			}
+		}
+
+		if (Input.GetButtonDown ("Fire1")) 
+		{
+			GetComponent<BoxCollider>().enabled = true;
+			m_LightDuration = m_MaxLightDuration;
+		}
+	}
+}
